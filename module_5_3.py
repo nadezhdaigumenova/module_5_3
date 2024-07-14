4b3c45f2fb7d21d3ef25e5a00fba6502c7498e9a
nadezhdaigumenova <nadya.kyzlakova@mail.ru>
2024-07-14 10:00:49 +0700

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -1,54 +1,28 @@
 class House:
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def __str__(self):
-        return (f'Название: {self.name}, количество этажей: {self.number_of_floors}')
-
-    def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
-
-    def __add__(self, value):
-        self.number_of_floors += value
-        return self
-
-    def __iadd__(self, value):
-        self.number_of_floors += value
-        return self
-    def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
-
-    def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
-
-    def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
-
-    def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
-
-    def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
-
-
+    def __del__(self):
+        print(f'{self.name}, снесен, но он станется в истории')
 
 h1 = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация',20)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрешки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
-print(h1 == h2)
-h1 = h1 + 10
-print(h1)
-print(h1 == h2)
-h1 += 10
-print(h1)
-h2 = h2 + 10
-print(h2)
-print(h1 > h2)
-print(h1 >= h2)
-print(h1 < h2)
-print(h1 <= h2)
-print(h1 != h2)
+del h2
+del h3
+print(House.houses_history)
+
+
+
+
 
